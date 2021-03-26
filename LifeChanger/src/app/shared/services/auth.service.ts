9d0352924ab1b4c1351bf0fa800fedd7e8165ab7
Kapi <kacper.791@o2.pl>
2021-03-26 15:41:29 +0100

    dodanie logowania

--- LifeChanger/src/app/shared/services/auth.service.ts
+++ LifeChanger/src/app/shared/services/auth.service.ts
@@ -8,21 +8,20 @@ import { JwtHelperService } from '@auth0/angular-jwt'
 })
 
 export class AuthService {
-  authUrl = "localhost:5001/api/account/testauth/";
-  userUrl = "localhost:5001/api/account/register/";
-  confirmEmailUrl = "localhost:5001/api/account/confirm-email/";
-  changePasswordUrl = "localhost:5001/api/account/change-password/"
+  authUrl = "https://localhost:5001/api/Account/";
+  userUrl = "https://localhost:5001/api/Account/";
+  confirmEmailUrl = "localhost:5001/api/Account/confirm-email/";
+  changePasswordUrl = "localhost:5001/api/Account/change-password/";
   helper = new JwtHelperService();
-
+  token;
 
   constructor(private http: HttpClient) { }
 
   login(model: any) {
     console.log(model);
     return this.http.post(this.authUrl + 'login', model).pipe(
-      map((response: any) => {
-        const user = response;
-        if (user.result.succeded) {
+      map((user: any) => {
+        if (user) {
           localStorage.setItem('token', user.token)
         }
       })
@@ -33,7 +32,7 @@ export class AuthService {
       'confirmEmailUrl': this.confirmEmailUrl
     });
     let options = { headers: headers };
-    return this.http.post(this.userUrl + 'create', model, options)
+    return this.http.post(this.userUrl + 'register', model, options)
   }
 
   resetPassword(model: any) {
@@ -52,10 +51,9 @@ export class AuthService {
     return this.http.post(this.authUrl + "changepassword", model); // dodac koncowke urla confirmmail
   }
 
-  loggedIn(): boolean {
-    // this.token = localStorage.getItem('token');
-    // return this.helper.isTokenExpired(token);
-    return true;
+  loggedIn() {
+    this.token = localStorage.getItem('token');
+    return !this.helper.isTokenExpired(this.token);
   }
 }
 
