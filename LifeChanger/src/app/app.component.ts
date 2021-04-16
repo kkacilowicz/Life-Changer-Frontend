de43ad55978339ea73de18ecf766cf9b14761e47
Kapi <kacper.791@o2.pl>
2021-04-16 16:40:14 +0200

    delete/modify strange files

--- LifeChanger/src/app/app.component.ts
+++ LifeChanger/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './shared/services/auth.service';
 import { JwtHelperService } from "@auth0/angular-jwt";
-// import { IUser } from './shared/models/user';
 
 @Component({
   selector: 'app-root',
@@ -18,4 +17,5 @@ export class AppComponent implements OnInit {
       this.authService.decodedToken = this.helper.decodeToken(token);
     }
   }
+
 }
--- LifeChanger/src/app/app.module.ts
+++ LifeChanger/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
@@ -9,7 +8,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { PreferencesComponent } from './pages/preferences/preferences.component';
 import { UserDashboardModule } from './user-dashboard/user-dashboard.module';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
@@ -25,7 +24,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     SharedModule,
     AuthModule,
     UserDashboardModule,
-    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
