21ce757ded9228421a043fd51255a7b00e31a803
WJakubowska <nika99_11@o2.pl>
2021-04-22 00:39:38 +0200

    dziala wysylanie samego love

--- LifeChanger/src/app/pages/preferences/love/love.component.ts
+++ LifeChanger/src/app/pages/preferences/love/love.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { PreferencesService } from 'src/app/shared/services/preferences.service';
+
+
 
 @Component({
   selector: 'app-love',
@@ -7,9 +11,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoveComponent implements OnInit {
 
-  constructor() { }
+  loveImages = ['../../../assets/Images/Preferences/love/jedzenie.jpg', '../../../assets/Images/Preferences/love/rzezba.jpg',
+    '../../../assets/Images/Preferences/love/kino.jpg', '../../../assets/Images/Preferences/love/film.jpg',
+    '../../../assets/Images/Preferences/love/rower.jpg', '../../../assets/Images/Preferences/love/spacer.jpg',]
+
+
+
+  constructor(
+    public Preferences: PreferencesService, 
+    public fb: FormBuilder
+  ) { }
+
+
+  form!: FormGroup;   // form 
+  selectedDetails: number[] = [];  // list of selected items
+  option1 = 1;
+  option2 = 28;
+  option3 = 37;
+  option4 = 50;
+  option5 = 32;
+  option6 = 31;
 
   ngOnInit(): void {
+    this.selectedDetails = new Array<number>();
+    this.form = this.fb.group({
+      categories: [''],
+    })
   }
+
+  onSubmit() {
+    const detailsObserver = {
+      next: x => {
+        console.log('Details love OK');
+      },
+      error: err => {
+        console.log(err);
+      }
+    };
+
+    this.form.patchValue({ categories: this.selectedDetails })
+    console.log(this.selectedDetails)
+    this.Preferences.details(this.form.value).subscribe(detailsObserver);
+  }
+
+  getDetailId(e: any, id: number) {
+    if (e.target.checked) {
+      this.selectedDetails.push(id);
+    }
+    else {
+      this.selectedDetails = this.selectedDetails.filter(m => m != id);
+    }
+    return this.selectedDetails;
+  }
+
+
 
 }
