12ab6e93338d92af495fb2084c7052f47a104b4e
WJakubowska <nika99_11@o2.pl>
2021-04-22 01:07:37 +0200

    sport ok z wysylaniem

--- LifeChanger/src/app/pages/preferences/culture/culture.component.ts
+++ LifeChanger/src/app/pages/preferences/culture/culture.component.ts
@@ -7,7 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CultureComponent implements OnInit {
 
+  cultureImage = ['../../../assets/Images/Preferences/culture/film.jpg','../../../assets/Images/Preferences/culture/gry.jpg',
+                  '../../../assets/Images/Preferences/culture/impreza.jpg','../../../assets/Images/Preferences/culture/koncert.jpg',
+                  '../../../assets/Images/Preferences/culture/ksiazka.jpg','../../../assets/Images/Preferences/culture/sluchawki.jpg',
+                  '../../../assets/Images/Preferences/culture/teatr.jpg',
+                  ]             
+
+
   constructor() { }
+
+  option16 = 9;
+  option17 = 10;
+  option18 = 11;
+  option19 = 13;
+  option20 = 14;
+  option21 = 15;
+  option22 = 16;
 
   ngOnInit(): void {
   }
--- LifeChanger/src/app/pages/preferences/sport/sport.component.ts
+++ LifeChanger/src/app/pages/preferences/sport/sport.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { PreferencesService } from 'src/app/shared/services/preferences.service';
 
 @Component({
   selector: 'app-sport',
@@ -7,9 +9,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SportComponent implements OnInit {
 
-  constructor() { }
+
+  sportImage = ['../../../assets/Images/Preferences/sport/basen.jpg','../../../assets/Images/Preferences/sport/aerobik.jpg',
+  '../../../assets/Images/Preferences/sport/relaks.jpg','../../../assets/Images/Preferences/sport/rower.jpg',
+  '../../../assets/Images/Preferences/sport/spacer.jpg','../../../assets/Images/Preferences/sport/nordic.jpg',
+  '../../../assets/Images/Preferences/sport/rolki.jpg','../../../assets/Images/Preferences/sport/bieg.jpg',
+  '../../../assets/Images/Preferences/sport/silownia.jpg',]
+
+
+  constructor(
+    public Preferences: PreferencesService, 
+    public fb: FormBuilder
+  ) { }
+
+  form!: FormGroup;   // form 
+  selectedDetails: number[] = [];  // list of selected items
+  option1 = 2;
+  option2 = 3;
+  option3 = 4;
+  option4 = 5;
+  option5 = 6;
+  option6 = 7;
+  option7 = 8;
+  option8 = 26;
+  option9 = 27;
+
 
   ngOnInit(): void {
+    this.selectedDetails = new Array<number>();
+    this.form = this.fb.group({
+      categories: [''],
+    })
   }
+
+  onSubmit() {
+    const detailsObserver = {
+      next: x => {
+        console.log('Details love OK');
+      },
+      error: err => {
+        console.log(err);
+      }
+    };
+
+    this.form.patchValue({ categories: this.selectedDetails })
+    console.log(this.selectedDetails)
+    this.Preferences.details(this.form.value).subscribe(detailsObserver);
+  }
+
+  getDetailId(e: any, id: number) {
+    if (e.target.checked) {
+      this.selectedDetails.push(id);
+    }
+    else {
+      this.selectedDetails = this.selectedDetails.filter(m => m != id);
+    }
+    return this.selectedDetails;
+  }
+
+
+
 
 }
