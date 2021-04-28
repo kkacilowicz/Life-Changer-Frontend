a5c69fb296b328ba0954bd689f0e23ab96bb9efa
WJakubowska <nika99_11@o2.pl>
2021-04-28 11:23:48 +0200

    preferencje po zalogowaniu jesli nie wybrane

--- LifeChanger/src/app/pages/home/home.component.ts
+++ LifeChanger/src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(    public authService: AuthService,
+    public userService: UserService, ) { }
 
   ngOnInit(): void {
   }
