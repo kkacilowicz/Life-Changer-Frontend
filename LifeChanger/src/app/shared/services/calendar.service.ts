e44bb7b3bfa692c2e60e12ce5276aae7befb38c7
Kapi <kacper.791@o2.pl>
2021-05-09 22:43:12 +0200

    uzywanie pobranego id dziala

--- LifeChanger/src/app/shared/services/calendar.service.ts
+++ LifeChanger/src/app/shared/services/calendar.service.ts
@@ -38,8 +38,8 @@ export class CalendarService {
     return number = number < 10 ? `0${number}`:number;
   }
 
-  createEvent(summary,startDate, endDate){
-
+  createEvent(summary,startDate, endDate, calID){
+    console.log(this.calendarID);
     const minDate = {
       year: startDate.getFullYear(),
       month: startDate.getMonth()+1,
@@ -71,8 +71,8 @@ export class CalendarService {
     this.event.summary = summary;
     this.event.start.dateTime = `${minDate.year}-${minDate.month}-${minDate.day}T${minTime.hour}:${minTime.minutes}:00+02:00`;
     this.event.end.dateTime = `${maxDate.year}-${maxDate.month}-${maxDate.day}T${maxTime.hour}:${maxTime.minutes}:00+02:00`;
-    this.sendEvent(this.calendarID, this.event).subscribe(response =>{
-      console.log('Dodano event');
+    this.sendEvent(calID, this.event).subscribe(response =>{
+      // console.log('Dodano event');
     })
   }
 
@@ -97,32 +97,18 @@ export class CalendarService {
     return this.httpClient.post(this.calendarApi + 'updatecalendar', JSON.stringify({token: pickedCalendarId}), { headers: reqHeader });
   }
 
-  getChoosenCalendarId(){
-    const token = localStorage.getItem('token')
-    let reqHeader = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + token,
-      })
-    this.httpClient.get<string>(this.calendarApi + 'calendar', { headers: reqHeader }).subscribe(response =>{
-      console.log(response);
-      this.calendarID = (response = null)? ' ': response;
-      //${this.calendarID} chwilowo nie dziala potem dodam -> chodzi o wybrane id wczesniej
-      this.calendarUrl = `https://calendar.google.com/calendar/embed?src=kacper.791@o2.pl&ctz=Europe%2FWarsaw&wkst=1&bgcolor=%23ffffff&showPrint=0&showCalendars=0`
-    });
-  }
-
-
   sendEvent(calendarID, event){
+    // console.log(`event: ${event}, calendarID: ${calendarID}`);
     let reqHeader = new HttpHeaders({
       'Content-Type': 'application/json',
       'Authorization': 'Bearer ' + this.accessToken,
       });
       //cgr1d92tq6dtp81reav8caubbc@group.calendar.google.com
-    // return this.httpClient.post(`https://www.googleapis.com/calendar/v3/calendars/${calendarID}/events`, this.event, { headers: reqHeader });
-    return this.httpClient.post(`https://www.googleapis.com/calendar/v3/calendars/cgr1d92tq6dtp81reav8caubbc@group.calendar.google.com/events`, this.event, { headers: reqHeader });
+    return this.httpClient.post(`https://www.googleapis.com/calendar/v3/calendars/${calendarID}/events`, this.event, { headers: reqHeader });
+    // return this.httpClient.post(`https://www.googleapis.com/calendar/v3/calendars/cgr1d92tq6dtp81reav8caubbc@group.calendar.google.com/events`, this.event, { headers: reqHeader });
   }
 
-  getCalendarEvents(minTime, maxTime):Observable<any>{
+  getCalendarEvents(minTime, maxTime, calID):Observable<any>{
     minTime.month = this.checkNumber(minTime.month);
     minTime.day = this.checkNumber(minTime.day);
     minTime.hour = this.checkNumber(minTime.hour);
@@ -136,11 +122,11 @@ export class CalendarService {
       'Content-Type': 'application/json',
       'Authorization': 'Bearer ' + this.accessToken,
       });
-    // return this.httpClient.get<any>(`https://www.googleapis.com/calendar/v3/calendars/${this.calendarID}/events`);
-    return this.httpClient.get<any>(`https://www.googleapis.com/calendar/v3/calendars/kacper.791@o2.pl/events?timeMax=${maxTime.year}-${maxTime.month}-${maxTime.day}T${maxTime.hour}%3A${maxTime.minute}%3A00%2B02%3A00&timeMin=${minTime.year}-${minTime.month}-${minTime.day}T${minTime.hour}%3A${minTime.minute}%3A00%2B02%3A00`,{ headers: reqHeader });
+
+    return this.httpClient.get<any>(`https://www.googleapis.com/calendar/v3/calendars/${calID}/events?timeMax=${maxTime.year}-${maxTime.month}-${maxTime.day}T${maxTime.hour}%3A${maxTime.minute}%3A00%2B02%3A00&timeMin=${minTime.year}-${minTime.month}-${minTime.day}T${minTime.hour}%3A${minTime.minute}%3A00%2B02%3A00`,{ headers: reqHeader });
   }
 
-  eventsToArray(){
+  eventsToArray(calID){
     const minTime = {
       year: this.date.getFullYear(),
       month: this.date.getMonth() + 1,
@@ -156,7 +142,7 @@ export class CalendarService {
       minute: 0
     };
 
-    this.getCalendarEvents(minTime, maxTime).subscribe(response =>{
+    this.getCalendarEvents(minTime, maxTime, calID).subscribe(response =>{
       for(let i = 0 ; i < response.items.length; i++)
       {
         let minTimeDate = new Date(response.items[i].start.dateTime);
@@ -169,17 +155,27 @@ export class CalendarService {
           dateEnd:`${maxTimeDate.getDate()}.${maxTimeDate.getMonth()+1}.${maxTimeDate.getFullYear()}`,
           timeEnd:`${maxTimeDate.getHours()}:${maxTimeDate.getMinutes()}`
           })
-        console.log(this.eventArray[i]);
       }
-      this.giveCalendarEvents(this.eventArray);
-      // console.log(response.items[0]);
-      // console.log(`Summary: ${response.items[0].summary}`);
-      // console.log(`Start Time: ${response.items[0].start.dateTime}`);
-      // console.log(`End Time: ${response.items[0].end.dateTime}`);
+      this.giveCalendarEvents(this.eventArray, calID);
     })
   }
 
-  giveCalendarEvents(eventArr){
+  getChoosenCalendarId(){
+    const token = localStorage.getItem('token')
+    let reqHeader = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + token,
+      })
+    this.httpClient.get<any>(this.calendarApi + 'calendar', { headers: reqHeader }).subscribe(response =>{
+      this.calendarID = response.token;
+      // console.log(`Id kalendarza: ${this.calendarID}`);
+      this.eventsToArray(this.calendarID);
+      this.calendarUrl = `https://calendar.google.com/calendar/embed?src=${this.calendarID}&wkst=1&bgcolor=%23ffffff&showPrint=0&showCalendars=0`
+      
+    });
+  }
+
+  giveCalendarEvents(eventArr, calID){
     const startDate = {
       day: '',
       month: '',
@@ -191,20 +187,11 @@ export class CalendarService {
       'Authorization': 'Bearer ' + token,
       })
     this.httpClient.post<any>(this.sendEventUrl, eventArr, { headers: reqHeader }).subscribe(response => {
-      console.log(response);
-      // for(let i = 0; i < response.length; i++)
-      // {
-      //   let eventTime = new Date(response);
-      //   createEvent(summary,startDate, endDate, startTime, endTime);
-      //   this.createEvent(response.name,startDate, endDate, startTime, endTime);
-      // }
       let startDateString = `${response.dateStart}T${response.timeStart}:00`;
       let endDateString = `${response.dateEnd}T${response.timeEnd}:00`;
       let eventStartTime = new Date(startDateString);
       let eventEndTime = new Date(endDateString);
-      console.log(eventStartTime);
-      console.log(eventEndTime);
-      this.createEvent(response.name, eventStartTime, eventEndTime);
+      this.createEvent(response.name, eventStartTime, eventEndTime, calID);
     })
   };
 
