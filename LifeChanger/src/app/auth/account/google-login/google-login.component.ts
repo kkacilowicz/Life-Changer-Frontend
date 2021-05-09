1f2d220391bd69186beb1468e2398e5d838b18a4
WJakubowska <nika99_11@o2.pl>
2021-05-09 21:06:44 +0200

    Dodanie przechodzenia

--- LifeChanger/src/app/auth/account/google-login/google-login.component.ts
+++ LifeChanger/src/app/auth/account/google-login/google-login.component.ts
@@ -5,6 +5,7 @@ import { AlertService } from 'ngx-alerts';
 import { AuthService } from 'src/app/shared/services/auth.service';
 import { CalendarService } from 'src/app/shared/services/calendar.service';
 import { ProgressBarService } from 'src/app/shared/services/progress-bar.service';
+import { UserService } from 'src/app/shared/services/user.service'
 
 
 @Component({
@@ -14,7 +15,7 @@ import { ProgressBarService } from 'src/app/shared/services/progress-bar.service
 })
 export class GoogleLoginComponent implements OnInit {
 
-  constructor(public authService: AuthService, private alertService: AlertService, public progressBar: ProgressBarService, private socialAuthService: SocialAuthService, private calendarService: CalendarService) { }
+  constructor(private userService:UserService ,public authService: AuthService, private alertService: AlertService, public progressBar: ProgressBarService, private socialAuthService: SocialAuthService, private calendarService: CalendarService) { }
 
   ngOnInit(): void {
     this.socialAuthService.authState.subscribe((user) => {
@@ -38,7 +39,12 @@ export class GoogleLoginComponent implements OnInit {
 
   sendToken(){
     if(this.authService.sendGoogleToken().subscribe()){
-      this.authService.changePage('main');
+      if (this.userService.userPref.categories.length==0){
+        this.authService.changePage('preferences')
+      }
+      else{
+        this.authService.changePage('main');
+      }
       localStorage.setItem('accessToken', this.authService.User.response.access_token);
     }
 
