27e407aa34f9039da93fbb09b8b7e0fe31234449
WJakubowska <nika99_11@o2.pl>
2021-05-17 15:50:18 +0200

    logout ok

--- LifeChanger/src/app/pages/logout/logout.component.ts
+++ LifeChanger/src/app/pages/logout/logout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-logout',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogoutComponent implements OnInit {
 
-  constructor() { }
+  constructor( public authService: AuthService ) { }
 
   ngOnInit(): void {
   }
