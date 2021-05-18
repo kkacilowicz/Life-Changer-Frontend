362f041aeb9dbb85536bc5f01b4f12ce71038c1d
Kapi <kacper.791@o2.pl>
2021-05-18 17:32:43 +0200

    trying to resolve problems

--- LifeChanger/src/app/shared/services/user.service.ts
+++ LifeChanger/src/app/shared/services/user.service.ts
@@ -6,95 +6,82 @@ import { map } from 'rxjs/operators';
 import { userPreferences } from '../models/userPreferences';
 import { Router } from '@angular/router';
 
-
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
   userUrl = environment.apiUrl;
   preUrl = environment.preUrl;
 
-
   userPref: userPreferences = {
-    userName!: '',
-    categories!: [ ],
-  } 
+    userName: '',
+    categories: [],
+  };
 
-  constructor(private httpClient: HttpClient, private router: Router) { }
+  constructor(private httpClient: HttpClient, private router: Router) {}
 
-
-
-
-  getPreferences(): Observable<userPreferences>{
+  getPreferences(): Observable<userPreferences> {
     let headers = new HttpHeaders({
-      'Authorization': `Bearer ${localStorage.getItem('token')}`
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
     });
-    return this.httpClient.get<userPreferences>(this.preUrl + 'UserCategories', { headers: headers })
+    return this.httpClient.get<userPreferences>(
+      this.preUrl + 'UserCategories',
+      { headers: headers }
+    );
   }
 
-
-
-
- preferences():Promise<void>{
-
+  preferences(): Promise<void> {
     let resolveRef;
     let rejectRef;
 
     //create a new promise. Save the resolve and reject reference
     let dataPromise: Promise<void> = new Promise((resolve, reject) => {
-        resolveRef = resolve;
-        rejectRef = reject;
+      resolveRef = resolve;
+      rejectRef = reject;
     });
 
-    this.getPreferences()
-    .subscribe( (data: userPreferences) => {
-          this.userPref = {
-        userName : (data as any ).userName,
-        categories: (data as any).categories,  
-        };
-     resolveRef(null);
+    this.getPreferences().subscribe((data: userPreferences) => {
+      this.userPref = {
+        userName: (data as any).userName,
+        categories: (data as any).categories,
+      };
+      resolveRef(null);
     });
-    
-
-
-
-
-     return dataPromise
+    return dataPromise;
   }
 
   changePagePreferences(path: string) {
     this.router.navigateByUrl(path);
   }
 
-  checkPreferences(){
-    if(localStorage.getItem('token')){
-    this.preferences().then( () =>{
-    if (this.userPref.categories.length == 0) {
-      this.changePagePreferences('preferences')
-    } else {
-      this.changePagePreferences('main')
+  async checkPreferences() {
+    console.log('Weszlo do check preferences');
+    if (localStorage.hasOwnProperty('token')) {
+      console.log('wywolane jest user Service');
+      await this.preferences().then(() => {
+        if (this.userPref.categories.length == 0) {
+          this.changePagePreferences('preferences');
+        } else {
+          this.changePagePreferences('main');
+        }
+      });
     }
-    });
   }
-  }
-
-
 
   updateUser(model: any) {
     console.log(model);
     let headers = new HttpHeaders({
-      'Authorization': `Bearer ${localStorage.getItem('token')}`
-    })
-      .set('content-type', 'application/json-patch+json');
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    }).set('content-type', 'application/json-patch+json');
     let options = { headers: headers };
-    return this.httpClient.patch(this.userUrl + 'updateuser', model, options).pipe(
-      map((user: any) => {
-        if (user) {
-          localStorage.setItem('token', user.token);
-        }
-      })
-    )
+    return this.httpClient
+      .patch(this.userUrl + 'updateuser', model, options)
+      .pipe(
+        map((user: any) => {
+          if (user) {
+            localStorage.setItem('token', user.token);
+          }
+        })
+      );
   }
 }
-
